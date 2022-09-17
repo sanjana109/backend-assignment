a006a0d0a4335b23c9b7f063c6ee721b71f07e0c
sanjana109 <69433758+sanjana109@users.noreply.github.com>
2022-09-17 23:34:13 +0530

    renamed procfile

--- tvprojectnew/settings.py
+++ tvprojectnew/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-#import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -151,4 +151,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
