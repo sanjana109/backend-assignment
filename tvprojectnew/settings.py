d9721a4048942e2155a1e1085108aa46c729cb26
sanjana109 <69433758+sanjana109@users.noreply.github.com>
2022-09-17 20:51:00 +0530

    update removed .env

--- tvprojectnew/settings.py
+++ tvprojectnew/settings.py
@@ -22,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-nu02w5rqc-6pqz86kjyz^@vam5ofis0m4+y0hfub_obt^v8)8_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
